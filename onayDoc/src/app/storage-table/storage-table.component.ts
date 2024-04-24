63e21ad77ca1d3577a14520cb6ae0de1d72c5057
jeanseven <virgozhan@gmail.com>
2024-04-24 15:57:26 +0500

    build project on angular 17

---
+++ onayDoc/src/app/storage-table/storage-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-storage-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './storage-table.component.html',
+  styleUrl: './storage-table.component.scss'
+})
+export class StorageTableComponent {
+
+}
